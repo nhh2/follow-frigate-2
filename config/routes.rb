8165b930eef0d3477a57dab781a9caa340b353dc
nhh2 <nhernand@chicagobooth.edu>
2023-03-12 17:04:46 +0000

    Version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
 
+  # Routes for the Post resource:
 
+  # CREATE
+  post("/insert_post", { :controller => "posts", :action => "create" })
+          
+  # READ
+  get("/posts", { :controller => "posts", :action => "index" })
+  
+  get("/posts/:path_id", { :controller => "posts", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
+  
+  # DELETE
+  get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
+
+  #------------------------------
+
+  # Homepage
+    get("/",{ :controller => "application", :action => "writing_index" })
 
 end
